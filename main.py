90e19ac20e8c6313bfc273b622cbcb2bc2eb36c2
jakekasan <jake.kasan@gmail.com>
2018-10-12 16:48:38 +0100

    added get_tokens, get_lemms and remove_stopwords

---
+++ main.py
@@ -0,0 +1,34 @@
+import re
+import nltk
+
+from nltk.corpus import stopwords as nltk_stopwords
+
+def get_tokens(target=None):
+    """
+        takes a string or array and returns an array of all words, lowercase 
+    """
+    if target is None:
+        return []
+    
+    target = re.sub(r'/W'," ",target).split()
+
+    return target
+    
+def get_lemms(target=None):
+    """
+        lemmatizes array of strings
+
+        returns an array of lemms
+    """
+
+def remove_stopwords(target=None,stop_words=None):
+    """
+        removes a given set of stopwords
+    """
+    if stop_words is None:
+        stop_words = set(nltk_stopwords.stopwords('english'))
+
+    return [x for x in target if x not in stop_words]
+
+
+    
