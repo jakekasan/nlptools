b93ffc0e2e338cd4fc1bf14ee11ea92e1ccf940c
jakekasan <jake.kasan@gmail.com>
2018-10-13 22:50:31 +0100

    added word-freq function and started working on a pipeline

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from functools import reduce
+import pandas as pd
 import re
 import nltk
 
@@ -10,11 +12,15 @@ def get_tokens(target=None):
     """
     if target is None:
         return []
+
+    if type(target) == pd.Series:
+        target = reduce(lambda x,y: x+y,target)
     
     target = re.sub(r'/W'," ",target).split()
 
     return target
     
+
 def get_lemms(target=None):
     """
         lemmatizes array of strings
@@ -36,5 +42,22 @@ def remove_stopwords(target=None,stop_words=None):
 
     return [x for x in target if x not in stop_words]
 
+def get_word_freq(target=None,least_common=1000):
+    fd = nltk.FreqDist(target)
+    fd = list(fd)[-least_common:]
+    return fd
+
+def apply_nlp_treatment(df=None,text_col=None,words=1000):
+    if df is None or text_col is None:
+        return None
+
+    # get tokens of all words
+    tokens = get_tokens(target=df[text_col])
+
+    # remove stop words
+    tokens_no_stop = remove_stopwords(target=tokens)
+
+    # least common words
+    freq = get_word_freq(target=tokens_no_stop,least_common=2000)
 
     
