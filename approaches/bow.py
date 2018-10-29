e2811749e60a80058c12fb618a9a09b674fecc7a
jakekasan <jake.kasan@gmail.com>
2018-10-29 13:53:19 +0000

    added test for build function

--- approaches/bow.py
+++ approaches/bow.py
@@ -22,10 +22,10 @@ class BagOfWords(Approach):
             once it is built, the class can be used to transform further
             raw text into a numerical form.
         """
-        documents = self.tokenize(self.rawData)
-        self.documents = self.lemmatizeDocument(documents)
+        documents = self.tokenize_corpus(corpus,pos=True)
+        self.documents = self.lemmatize_corpus(documents)
 
-        self.bags = list(map(lambda x: individualBags(x),self.documents))
+        self.bags = [self.individual_bags(x) for x in self.documents]
 
         prototype = {}
         for bag in self.bags:
@@ -43,18 +43,18 @@ class BagOfWords(Approach):
         prototype = {a:b for a,b in prototype}
 
         self.totalBag = prototype
-        self.prototype = prototype.keys()
+        self.prototype = sorted(prototype.keys())
 
     def variablesFromRaw(self,raw):
         """
             returns an array of variables for a given array
         """
-        document = self.processRaw(raw)
-        bag = self.individualBags(document)
+        document = self.process_document(raw,pos=True)
+        bag = self.individual_bags(document)
 
         return [bag[x] if x in bag.keys() else 0 for x in self.prototype]
 
-    def individualBags(self,tokens):
+    def individual_bags(self,tokens):
         """
             takes a tokens of a document
             returns a dictionary of bag of words
@@ -68,6 +68,23 @@ class BagOfWords(Approach):
         return bag
 
 
+sample_corpus = ["This is a sentence","This is another sentence","And here is a phrase"]
+corpus_bow = {
+    "this":2,
+    "is":3,
+    "a":2,
+    "sentence":2,
+    "another":1,
+    "and":1,
+    "here":1,
+    "phrase":1
+}
+
+test_text = "This is a sentence"
+test_result = [1 if y in [x.lower() for x in test_text.split(" ")] else 0 for y in sorted(corpus_bow.keys())]
+
+
+
 class BOWTest(unittest.TestCase):
 
     def setUp(self):
@@ -76,14 +93,31 @@ class BOWTest(unittest.TestCase):
     def test_class(self):
         self.assertIsInstance(self.bow,BagOfWords)
 
-    def test_individualBags(self):
+    def test_individual_bags(self):
         """
             A comment!
         """
         string = ["Heavy", "hangs", "the", "head"]
         desired = {"Heavy":1,"hangs":1,"the":1,"head":1}
-        actual = self.bow.individualBags(string)
+        actual = self.bow.individual_bags(string)
         self.assertEqual(desired,actual)
+
+    def test_build(self):
+        print(sample_corpus)
+        self.bow.build(corpus=sample_corpus)
+
+        actual = self.bow.variablesFromRaw(test_text)
+        desired = [[1, 0, 0, 0, 1, 0, 1, 1]]
+
+        self.assertEqual(actual,desired)
+
+
+
+    
+
+
+
+    
 
     
 
