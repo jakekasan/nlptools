3c14c7257c5b3079e80453cf22211e1091499f4d
jakekasan <jake.kasan@gmail.com>
2018-10-29 13:52:50 +0000

    added treebank to wordnet tag translating

--- approaches/approach.py
+++ approaches/approach.py
@@ -5,6 +5,7 @@
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords as nltk_stopwords
+from nltk.corpus import wordnet as wn
 from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 
@@ -14,6 +15,7 @@ class Approach:
     def __init__(self,name=None,corpus=None):
         self.name = name or "Untitled"
         self.corpus = corpus
+        self.lemmer = WordNetLemmatizer()
 
     def buildApproach(self,df=None):
         """
@@ -40,6 +42,7 @@ class Approach:
         #tokens = list(map(lambda x: x.lower(),tokens))
         tokens = [w.lower() for w in tokens]
         tokens_pos = pos_tag(tokens)
+        tokens_pos = [(a,self.get_wordnet_tag(b)) for a,b in tokens_pos]
         if pos:
             return tokens_pos
         return tokens
@@ -54,21 +57,87 @@ class Approach:
         #list(map(lambda x: self.tokenize_document(x,pos=pos),corpus))
 
 
-    def lemmatize_document(self,document):
+    def lemmatize_document(self,tokens):
         """
             returns lemms for single document
         """
 
-        lemmer = WordNetLemmatizer()
+        return [self.lemmer.lemmatize(a,b) for a,b in tokens]
+        
 
-        lemms = [lemmer(a,pos=b) for a,b in document]
-
-    def lemmatizeCorpus(self,corpus):
+    def lemmatize_corpus(self,corpus):
         """
             returns lemms for entire corpus
         """
-        lemms = list(map(lambda x: self.lemmatizeDocument(x),corpus))
-        return lemms
+        #lemms = list(map(lambda x: self.lemmatize_document(x),corpus))
+        return [self.lemmatize_document(x) for x in corpus]
+
+    def get_wordnet_tag(self,tag):
+        """
+            return wordnet tag for corresponding treebank tag
+        """
+        tag_map = {
+        'CC':None, # coordin. conjunction (and, but, or)  
+        'CD':wn.NOUN, # cardinal number (one, two)             
+        'DT':None, # determiner (a, the)                    
+        'EX':wn.ADV, # existential ‘there’ (there)           
+        'FW':None, # foreign word (mea culpa)             
+        'IN':wn.ADV, # preposition/sub-conj (of, in, by)   
+        'JJ':[wn.ADJ, wn.ADJ_SAT], # adjective (yellow)                  
+        'JJR':[wn.ADJ, wn.ADJ_SAT], # adj., comparative (bigger)          
+        'JJS':[wn.ADJ, wn.ADJ_SAT], # adj., superlative (wildest)           
+        'LS':None, # list item marker (1, 2, One)          
+        'MD':None, # modal (can, should)                    
+        'NN':wn.NOUN, # noun, sing. or mass (llama)          
+        'NNS':wn.NOUN, # noun, plural (llamas)                  
+        'NNP':wn.NOUN, # proper noun, sing. (IBM)              
+        'NNPS':wn.NOUN, # proper noun, plural (Carolinas)
+        'PDT':[wn.ADJ, wn.ADJ_SAT], # predeterminer (all, both)            
+        'POS':None, # possessive ending (’s )               
+        'PRP':None, # personal pronoun (I, you, he)     
+        'PRP$':None, # possessive pronoun (your, one’s)    
+        'RB':wn.ADV, # adverb (quickly, never)            
+        'RBR':wn.ADV, # adverb, comparative (faster)        
+        'RBS':wn.ADV, # adverb, superlative (fastest)     
+        'RP':[wn.ADJ, wn.ADJ_SAT], # particle (up, off)
+        'SYM':None, # symbol (+,%, &)
+        'TO':None, # “to” (to)
+        'UH':None, # interjection (ah, oops)
+        'VB':wn.VERB, # verb base form (eat)
+        'VBD':wn.VERB, # verb past tense (ate)
+        'VBG':wn.VERB, # verb gerund (eating)
+        'VBN':wn.VERB, # verb past participle (eaten)
+        'VBP':wn.VERB, # verb non-3sg pres (eat)
+        'VBZ':wn.VERB, # verb 3sg pres (eats)
+        'WDT':None, # wh-determiner (which, that)
+        'WP':None, # wh-pronoun (what, who)
+        'WP$':None, # possessive (wh- whose)
+        'WRB':None, # wh-adverb (how, where)
+        '$':None, #  dollar sign ($)
+        '#':None, # pound sign (#)
+        '“':None, # left quote (‘ or “)
+        '”':None, # right quote (’ or ”)
+        '(':None, # left parenthesis ([, (, {, <)
+        ')':None, # right parenthesis (], ), }, >)
+        ',':None, # comma (,)
+        '.':None, # sentence-final punc (. ! ?)
+        ':':None # mid-sentence punc (: ; ... – -)
+        }
+
+        for key,value in tag_map.items():
+            if value is None:
+                tag_map[key] = wn.NOUN
+
+        to_return = tag_map[tag]
+
+        if to_return is None:
+            to_return = wn.NOUN
+
+        return to_return
+
+
+
+
 
 class TestApproach(unittest.TestCase):
     def setUp(self):
