9c8c406a82dfdc9a3891dfb9914f250ed580e29d
jakekasan <jake.kasan@gmail.com>
2018-11-01 15:15:48 +0000

    processes nltk.corpus.movie_reviews using Tf-Idf and saves result to csv

--- test.py
+++ test.py
@@ -2,27 +2,93 @@ from approaches.tfidf import TFIDF
 
 import pandas as pd
 
-data_dir = "./../stock-sentiment/test.csv"
+# data_dir = "./../stock-sentiment/test.csv"
 
-df = pd.read_csv(data_dir).head(2)
+# df = pd.read_csv(data_dir)
 
-print(df.head())
 
+# train = df.sample(10)["text"].values
+# test = df.sample(2)["text"].values
+
+# print(df.shape)
+
+# tf = TFIDF()
+
+# #corpus = df["text"].values
+
+# #print(corpus)
+
+# print("Building...")
+
+# tf.build(corpus=train)
+
+# print("Approach built!")
+
+# #print(tf.prototype)
+
+# results = list(map(lambda x: tf.apply(x),test))
+
+# for row in results:
+#     print(row)
+
+# res_dict = {}
+# for i,word in enumerate(tf.prototype):
+#     res_dict[word] = [x[i] for x in results]
+
+
+
+# results_df = pd.DataFrame(res_dict)
+
+# results_df.to_csv("./results.csv")
+
+#print(list(filter(lambda x: x > 0.0,results)))
+
+#tf.build(corpus=)
+
+
+from nltk.corpus import movie_reviews
+
+def remove_bad(string):
+    string = string.replace("\n","")
+    string = string.replace("\'","'")
+    return string
+
+documents = [(remove_bad(movie_reviews.raw(fileid)), category) for category in movie_reviews.categories() for fileid in movie_reviews.fileids(category)]
+
+documents = documents[:200] + documents[-200:]
+
+corpus = [a for a,b in documents]
+labels = [b for a,b in documents]
 
 tf = TFIDF()
 
-corpus = df["text"].values
 
-#print(corpus)
-
-corpus = ["What is this?","This is it"]
+print("Building...")
 
 tf.build(corpus=corpus)
 
-print("Done!")
+print("Approach built!")
 
-print(tf)
+print(f"Length of prototype: {len(tf.prototype)}")
 
-print(tf.apply("What is up, big chief?"))
+#results = list(map(lambda x: tf.apply(x),corpus))
 
-#tf.build(corpus=)
+import multiprocessing.pool
+
+pool = multiprocessing.pool.ThreadPool(processes=500)
+
+results = pool.map(tf.apply,corpus)
+
+print("Finished working on test set")
+
+res_dict = {}
+for i,word in enumerate(tf.prototype):
+    res_dict[word] = [x[i] for x in results]
+
+res_dict["labels"] = labels
+
+results_df = pd.DataFrame(res_dict)
+
+results_df.to_csv("./movie_reviews.csv")
+
+print("done!")
